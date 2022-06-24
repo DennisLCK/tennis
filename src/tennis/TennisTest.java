d39fcbc13a47c37ee33ce147c842b1c698241593
Dennis Liu <dennis.ck.liu@gogoro.com>
2022-06-25 02:41:01 +0800

    add base template and import junit5

---
+++ src/tennis/TennisTest.java
@@ -0,0 +1,13 @@
+package tennis;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TennisTest {
+    @Test
+    void test() {
+        boolean isTure = true;
+        assertTrue(isTure);
+    }
+}
